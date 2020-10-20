55942d85fdde2d78c34e860c29ded7e923b2e039
Sina <s.j.semnani@gmail.com>
2020-10-20 13:33:00 -0700

    Add dataset download script and instructions

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# safe-elections
+# FAQ Answering System
+
+## Installation
+We reccommend creating a virtual environment first:
+1. Install virtualenv if you haven't already:
+`pip install virtualenv`
+2. Create a virtual environment called faq:
+`virtualenv faq`
+4. Activate the environment:
+`source faq/bin/activate`
+. Install required packages:
+`pip install -r requirements.txt`
+
+Every time you start a new terminal, you need to activate this virtual environment (step 4) again.
+
+## Downloading the paraphrase detection dataset
+Run the following command:
+`python download_dataset.py ./QQP`
---
+++ download_dataset.py
@@ -0,0 +1,36 @@
+""" Script for downloading GLUE data.
+"""
+
+import argparse
+import os
+import sys
+import urllib.request
+import zipfile
+
+TASK2PATH = {
+    "QQP": "https://firebasestorage.googleapis.com/v0/b/mtl-sentence-representations.appspot.com/o/data%2FQQP.zip?alt=media&token=700c6acf-160d-4d89-81d1-de4191d02cb5",
+}
+
+def download_and_extract(task, data_dir):
+    print("Downloading and extracting %s..." % task)
+    data_file = "%s.zip" % task
+    urllib.request.urlretrieve(TASK2PATH[task], data_file)
+    with zipfile.ZipFile(data_file) as zip_ref:
+        zip_ref.extractall(data_dir)
+    os.remove(data_file)
+    print("\tCompleted!")
+
+
+def main(arguments):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", help="directory to save data to", type=str, default="glue_data")
+    args = parser.parse_args(arguments)
+
+    if not os.path.isdir(args.data_dir):
+        os.mkdir(args.data_dir)
+
+    download_and_extract('QQP', args.data_dir)
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
